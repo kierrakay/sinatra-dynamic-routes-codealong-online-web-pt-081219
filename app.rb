825e26557b0539a03727c0bc7e8adaf1806e735a
Kierra M <k.merriman32@hotmail.com>
2019-11-24 23:02:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,8 +21,8 @@ get '/goodbye/:name' do
 end 
 
   get '/multiply/:num1/:num2' do 
-      @num1 = params[:num1].to_s
-    @num2 = params[:num2].to_s
+      @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
 "#{@num1} #{@num2}"
 end
 end
